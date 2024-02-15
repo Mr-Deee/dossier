37cbf4d3df47f4521e43612c4b87b7fbd5be9437
DANIEL <merchantdaniel8@gmail.com>
2024-02-15 05:23:18 +0000

    register123

--- lib/main.dart
+++ lib/main.dart
@@ -6,10 +6,15 @@ import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:provider/provider.dart';
-
+import 'firebase_options.dart';
 import 'models/clientuser.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
       MultiProvider(providers: [
         ChangeNotifierProvider<clientusers>(
