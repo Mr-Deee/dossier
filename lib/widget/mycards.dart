d91dc7d1715d9254ff69557e90168a1916b70bc5
mrnarterh <merchantdaniel8@gmail.com>
2024-04-15 21:22:55 +0000

    homepage 2

--- lib/widget/mycards.dart
+++ lib/widget/mycards.dart
@@ -1,7 +1,11 @@
+import 'dart:io';
 import 'dart:ui';
 
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class MyCard extends StatelessWidget {
   @override
@@ -166,6 +170,8 @@ class MyCard2 extends StatelessWidget {
               ],
             ),
           );
+
+
         });
   }
 }
@@ -176,3 +182,446 @@ class Cards {
   static const String kcard3 = 'assets/images/acc.jpg';
 }
 
+
+class NewVehicle extends StatefulWidget {
+  const NewVehicle({Key? key}) : super(key: key);
+
+  @override
+  State<NewVehicle> createState() => _NewVehicleState();
+}
+
+class _NewVehicleState extends State<NewVehicle> {
+  final DatabaseReference  _database = FirebaseDatabase.instance.reference();
+  final modelname = TextEditingController();
+  final vehiclenumber = TextEditingController();
+  final mobilenumber = TextEditingController();
+  final type = TextEditingController();
+  final price = TextEditingController();
+  final seat = TextEditingController();
+  final location = TextEditingController();
+
+  List<File?> _images = [];
+
+  List<String> _imageUrls = [];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white.withOpacity(0.8),
+      appBar: AppBar(
+        backgroundColor: Colors.white.withOpacity(0),
+        elevation: 0,
+        title: Text(
+          'Add Vehicle ',
+          style: TextStyle(
+            fontFamily: 'Bowlby',
+            color: Colors.black,
+            fontSize: MediaQuery.of(context).size.aspectRatio * 70,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: SafeArea(
+        child: ListView(
+          keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+          scrollDirection: Axis.vertical,
+          children: [
+            Column(
+              children: <Widget>[
+                // Your existing text fields...
+                // Add image picker button
+                Container(
+                  margin: EdgeInsets.only(top: 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      _imageSelectorContainer(0),
+                      _imageSelectorContainer(1),
+                      _imageSelectorContainer(2),
+                    ],
+                  ),
+                ),
+
+                Container(
+                  margin: EdgeInsets.only(top: 25),
+                  padding: EdgeInsets.all(10),
+                  child: TextField(
+                    controller: modelname,
+                    keyboardType: TextInputType.name,
+                    textInputAction: TextInputAction.next,
+                    decoration: InputDecoration(
+                      labelText: 'Model Name',
+                      suffixIcon: Icon(Icons.car_rental_rounded),
+                      floatingLabelStyle: TextStyle(
+                          color: Colors.blue,
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(17),
+                        ),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide:
+                        (BorderSide(width: 1.0, color: Colors.black)),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(17),
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide:
+                        (BorderSide(width: 1.0, color: Colors.blue)),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(17),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 8),
+                  padding: EdgeInsets.all(10),
+                  child: TextField(
+                    controller: vehiclenumber,
+                    keyboardType: TextInputType.text,
+                    textInputAction: TextInputAction.next,
+                    decoration: InputDecoration(
+                      labelText: 'Vehicle Number',
+                      suffixIcon: Icon(Icons.numbers_rounded),
+                      floatingLabelStyle: TextStyle(
+                          color: Colors.blue,
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(17),
+                        ),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide:
+                        (BorderSide(width: 1.0, color: Colors.black)),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(17),
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide:
+                        (BorderSide(width: 1.0, color: Colors.blue)),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(17),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 8),
+                  padding: EdgeInsets.all(10),
+                  child: TextField(
+                    controller: mobilenumber,
+                    keyboardType: TextInputType.phone,
+                    textInputAction: TextInputAction.next,
+                    decoration: InputDecoration(
+                      labelText: 'Mobile Number',
+                      suffixIcon: Icon(Icons.phone_android_rounded),
+                      floatingLabelStyle: TextStyle(
+                          color: Colors.blue,
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(17),
+                        ),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide:
+                        (BorderSide(width: 1.0, color: Colors.black)),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(17),
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide:
+                        (BorderSide(width: 1.0, color: Colors.blue)),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(17),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 8),
+                  padding: EdgeInsets.all(10),
+                  child: TextField(
+                    controller: type,
+                    keyboardType: TextInputType.text,
+                    textInputAction: TextInputAction.next,
+                    decoration: InputDecoration(
+                      labelText: 'Type',
+                      suffixIcon: Icon(Icons.select_all_sharp),
+                      floatingLabelStyle: TextStyle(
+                          color: Colors.blue,
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(17),
+                        ),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide:
+                        (BorderSide(width: 1.0, color: Colors.black)),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(17),
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide:
+                        (BorderSide(width: 1.0, color: Colors.blue)),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(17),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 8),
+                  padding: EdgeInsets.all(10),
+                  child: TextField(
+                    controller: seat,
+                    keyboardType: TextInputType.numberWithOptions(
+                        signed: false, decimal: false),
+                    textInputAction: TextInputAction.next,
+                    decoration: InputDecoration(
+                      labelText: 'No.of Seats',
+                      suffixIcon: Icon(Icons.chair),
+                      floatingLabelStyle: TextStyle(
+                          color: Colors.blue,
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(17),
+                        ),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide:
+                        (BorderSide(width: 1.0, color: Colors.black)),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(17),
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide:
+                        (BorderSide(width: 1.0, color: Colors.blue)),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(17),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 8),
+                  padding: EdgeInsets.all(10),
+                  child: TextField(
+                    controller: price,
+                    keyboardType: TextInputType.number,
+                    textInputAction: TextInputAction.next,
+                    decoration: InputDecoration(
+                      //labelText: 'Price',
+                      hintText: 'Price/Day',
+                      prefixIcon: Icon(Icons.attach_money),
+                      floatingLabelStyle: TextStyle(
+                          color: Colors.blue,
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(17),
+                        ),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide:
+                        (BorderSide(width: 1.0, color: Colors.black)),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(17),
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide:
+                        (BorderSide(width: 1.0, color: Colors.blue)),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(17),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 8),
+                  padding: EdgeInsets.all(10),
+                  child: TextField(
+                    controller: location,
+                    keyboardType: TextInputType.text,
+                    textInputAction: TextInputAction.done,
+                    decoration: InputDecoration(
+                      labelText: 'Your Location',
+                      suffixIcon: Icon(Icons.pin_drop),
+                      floatingLabelStyle: TextStyle(
+                          color: Colors.blue,
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(17),
+                        ),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide:
+                        (BorderSide(width: 1.0, color: Colors.black)),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(17),
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide:
+                        (BorderSide(width: 1.0, color: Colors.blue)),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(17),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+
+                // ElevatedButton(
+                //   onPressed: () {
+                //     _pickImage(index);
+                //   },
+                //   child: Text('Select Image'),
+                // ),
+                SizedBox(height: 10),
+                // Display selected images
+
+                Container(
+                  margin: EdgeInsets.only(top: 10, left: 10),
+                  width: MediaQuery.of(context).size.width * 0.5,
+                  height: MediaQuery.of(context).size.height * 0.075,
+                  child: ElevatedButton(
+                    onPressed:(){
+
+                      addvehicle();
+                      // addVehicledb();
+                    },
+                    child: Text(
+                      'Confirm',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontFamily: 'DelaGothic',
+                      ),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      enableFeedback: false,
+                      elevation: 20,
+                      backgroundColor: Colors.blue,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  // void addvehicle() async {
+  //   // Upload images to Firebase Storage
+  //   for (var imageFile in _images) {
+  //     if (imageFile != null) {
+  //       // You need to implement Firebase initialization before this step
+  //       final storageRef =
+  //       firebase_storage.FirebaseStorage.instance.ref().child(
+  //         'vehicle_images/${DateTime.now().millisecondsSinceEpoch}.jpg',
+  //       );
+  //       await storageRef.putFile(imageFile);
+  //       // Get download URL of uploaded image (you can save this URL to Firestore or Realtime Database)
+  //       final String downloadURL = await storageRef.getDownloadURL();
+  //       // Here you can do something with the download URL, like saving it to Firestore
+  //       print('Download URL: $downloadURL');
+  //     }
+  //   }
+  //
+  //   // Rest of your code...
+  //   Navigator.pushReplacementNamed(context, '/homepage', arguments: {
+  //     'modelname': modelname.text,
+  //     'price': price.text,
+  //     'type': type.text,
+  //     'mobilenumber': mobilenumber.text,
+  //     'seat': seat.text,
+  //     'vehiclenumber': vehiclenumber.text,
+  //     'location': location.text,
+  //   });
+  // }
+
+  Widget _imageSelectorContainer(int index) {
+    return GestureDetector(
+      onTap: () {
+        _pickImage(index);
+      },
+      child: Container(
+        width: 100,
+        height: 100,
+        margin: EdgeInsets.all(8),
+        decoration: BoxDecoration(
+          color: Colors.grey.withOpacity(0.3),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Center(
+          child: _images.length > index && _images[index] != null
+              ? Image.file(_images[index]!)
+              : Icon(Icons.add),
+        ),
+      ),
+    );
+  }
+  void _pickImage(int index) async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+    if (pickedFile != null) {
+      setState(() {
+        if (_images.length < 3) {
+          _images.add(File(pickedFile.path));
+        } else {
+          // Show error message or limit reached message
+          // You can use a SnackBar for this purpose
+        }
+      });
+    }
+  }
+
+  void addvehicle() async {
+    // Upload images to Firebase Storage
+    for (var imageFile in _images) {
+      if (imageFile != null) {
+        final storageRef =
+        FirebaseStorage.instance.ref().child(
+          'vehicle_images/${DateTime
+              .now()
+              .millisecondsSinceEpoch}.jpg',
+        );
+        await storageRef.putFile(imageFile);
+        final downloadURL = await storageRef.getDownloadURL();
+        _imageUrls.add(downloadURL);
+      }
+    }
+  }
+
+
+}
